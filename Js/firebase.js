03010380427eb715fda8794bdfa48645664a4521
narsi-jangid <narsijangid01@gmail.com>
2025-08-01 13:08:59 +0530

    done

--- Js/firebase.js
+++ Js/firebase.js
@@ -5,7 +5,7 @@ import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, si
 
 const firebaseConfig = {
   apiKey: "AIzaSyBcjUtfNxD4fHv6uiHXAEkN8wXtQBkVbCA",
-  authDomain: "zair.netlify.app",
+  authDomain: "zairapp.vercel.app",
   databaseURL: "https://dazzlone-default-rtdb.firebaseio.com",
   projectId: "dazzlone",
   storageBucket: "dazzlone.appspot.com",
